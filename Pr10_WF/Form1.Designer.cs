1b1b8a25a642b0eceba7424167f4ad751936e825
Natalya <65410808+frants199@users.noreply.github.com>
2020-05-22 21:57:03 +0300

    Добавьте файлы проекта.

---
+++ Pr10_WF/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Pr10_WF
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.execute_button = new System.Windows.Forms.Button();
+			this.process_box = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// execute_button
+			// 
+			this.execute_button.Location = new System.Drawing.Point(0, 305);
+			this.execute_button.Name = "execute_button";
+			this.execute_button.Size = new System.Drawing.Size(411, 50);
+			this.execute_button.TabIndex = 3;
+			this.execute_button.Text = "Выполнить";
+			this.execute_button.UseVisualStyleBackColor = true;
+			this.execute_button.Click += new System.EventHandler(this.execute_button_Click);
+			// 
+			// process_box
+			// 
+			this.process_box.Dock = System.Windows.Forms.DockStyle.Top;
+			this.process_box.Location = new System.Drawing.Point(0, 0);
+			this.process_box.Name = "process_box";
+			this.process_box.Size = new System.Drawing.Size(413, 299);
+			this.process_box.TabIndex = 2;
+			this.process_box.Text = "";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(413, 362);
+			this.Controls.Add(this.execute_button);
+			this.Controls.Add(this.process_box);
+			this.Name = "Form1";
+			this.Text = "Практика 10";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button execute_button;
+		private System.Windows.Forms.RichTextBox process_box;
+	}
+}
+
---
+++ Pr10_WF/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Pr10_WF
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void info(DirectoryInfo directory, FileInfo K1_file1, FileInfo K1_file2, FileInfo K2_file1)
+		{
+			string str = "";
+			str += $"Directory '{directory.Name}'\n".ToUpper();
+			str += $"Full Name: {directory.FullName}\n" +
+								$"Name: {directory.Name}\n" +
+								$"Creation: {directory.CreationTime}\n" +
+								$"Attributes: {directory.Attributes}\n" +
+								$"Root: {directory.Root}\n";
+			str += $"File 't1'\n".ToUpper();
+			str += $"Full Name: {K1_file1.FullName}\n" +
+								$"Name: {K1_file1.Name}\n" +
+								$"Extention:{K1_file1.Extension}\n" +
+								$"Creation: {K1_file1.CreationTime}\n" +
+								$"Attributes: {K1_file1.Attributes}\n";
+			str += "File 't2'\n".ToUpper();
+			str += $"Full Name: {K1_file2.FullName}\n" +
+								$"Name: {K1_file2.Name}\n" +
+								$"Extention:{K1_file2.Extension}\n" +
+								$"Creation: {K1_file2.CreationTime}\n" +
+								$"Attributes: {K1_file2.Attributes}\n";
+			str += "File 't3'\n".ToUpper();
+			str += $"Full Name: {K2_file1.FullName}\n" +
+								$"Name: {K2_file1.Name}\n" +
+								$"Extention:{K2_file1.Extension}\n" +
+								$"Creation: {K2_file1.CreationTime}\n" +
+								$"Attributes: {K2_file1.Attributes}\n";
+			process_box.Text = str;
+		}
+		private void execute_button_Click(object sender, EventArgs e)
+		{
+			DirectoryInfo directory = new DirectoryInfo(@"C:\temp");
+			directory.Create();
+			directory.CreateSubdirectory("K1");
+			directory.CreateSubdirectory("K2");
+			FileInfo K1_file1 = new FileInfo(@"C:\temp\K1\t1.txt");
+			using (StreamWriter sw = K1_file1.CreateText())
+			{
+				sw.WriteLine("Иванов Иван Иванович, 1965 года рождения, место жительства г. Саратов");
+			}
+			FileInfo K1_file2 = new FileInfo(@"C:\temp\K1\t2.txt");
+			using (StreamWriter sw = K1_file2.CreateText())
+			{
+				sw.WriteLine("Петров Сергей Федорович, 1966 года рождения, место жительства г.Энгельс");
+			}
+			FileInfo K2_file1 = new FileInfo(@"C:\temp\K2\t3.txt");
+			string str = "", str2 = "";
+			using (StreamReader sr = K1_file1.OpenText())
+			{
+				var s = "";
+				while ((s = sr.ReadLine()) != null)
+				{
+					str += s;
+				}
+			}
+			using (StreamReader sr = K1_file2.OpenText())
+			{
+				var s = "";
+				while ((s = sr.ReadLine()) != null)
+				{
+					str2 += s;
+				}
+			}
+			using (StreamWriter sw = K2_file1.CreateText())
+			{
+				sw.WriteLine(str + "\n" + str2);
+			}
+			info(directory, K1_file1, K1_file2, K2_file1);
+			try
+			{
+				File.Move(@"C:\temp\K1\t2.txt", @"C:\temp\K2\t2.txt");
+				K1_file1.CopyTo(@"C:\temp\K2\t1.txt");
+				Directory.Delete(@"C:\temp\K1", true);
+				Directory.Move(@"C:\temp\K2", @"C:\temp\All");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			DirectoryInfo AllDirectory = new DirectoryInfo(@"C:\temp\All");
+			info(AllDirectory, K1_file1, K1_file2, K2_file1);
+			try
+			{
+				System.Diagnostics.Process.Start(@"C:\temp\All");
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+	}
+}
